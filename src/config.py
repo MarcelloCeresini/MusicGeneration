3a8e7eec15b722c150955e852085921588699831
MarcelloCeresini <mceresini.97@gmail.com>
2022-11-07 13:01:01 +0100

    Started writing representation

---
+++ src/config.py
@@ -0,0 +1,11 @@
+class Config:
+    def __init__(self, conf_string):
+        
+
+        # tempo definition
+        max_tempo = 256
+        min_tempo = 16
+        num_tempos = 49
+        r = (max_tempo/min_tempo)**(1/(num_tempos-1))
+
+        self.tempos = [min_tempo * r**i for i in range(num_tempos)]

92425349000faff885b5a62029f63cdae85f7aea
volpepe <volpepe.prog@gmail.com>
2022-11-26 18:22:03 +0100

    Cleaned up config for server training

--- src/config.py
+++ src/config.py
@@ -6,8 +6,22 @@ import math
 
 class Config:
     
-    def __init__(self, config_string, ROOT_PATH):
-        ### config_string for ablation
+    def __init__(self, config_string, root_path):
+
+        # SYSTEM INFO
+        self.N_CPUS  = os.cpu_count()
+        self.GPUS    = tf.config.experimental.list_physical_devices('GPU')
+        # Setup memory growth
+        for gpu in self.GPUS:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        # Setup training streategy for Multi-GPU training
+        if len(self.GPUS) > 1:
+            self.training_strategy = tf.distribute.MirroredStrategy()
+            self.num_devices = self.training_strategy.num_replicas
+        else:
+            self.num_devices = 1
+        
+        # DATASET SELECTION
         self.config_string = config_string
 
         if config_string == "complete":
@@ -17,8 +31,8 @@ class Config:
         else:
             raise ValueError("Not implemented yet")
 
-        # PATHS
-        self.DATA_PATH = os.path.join(ROOT_PATH, "data/")
+        # PATHS DEFINITION
+        self.DATA_PATH = os.path.join(root_path, "data/")
         self.dataset_paths = {
             "lmd": os.path.join(self.DATA_PATH, "lmd/"),
             "maestro": os.path.join(self.DATA_PATH, "maestro/"),
@@ -27,48 +41,42 @@ class Config:
             "folk": os.path.join(self.DATA_PATH, "folk/"),
             "lmd_matched": os.path.join(self.DATA_PATH, "lmd_matched/")
         }
+        self.tf_data_path = os.path.join(self.DATA_PATH, "tf_data")
+        self.lmda_genres_tf_data_path = os.path.join(self.DATA_PATH, "lmda_genres_tf_data")
 
-        self.N_CPUS = os.cpu_count()
-
-
-        # tempo definition
+        # NOTATION DEFINITIONS
+        # Tempo
         max_tempo = 256
         min_tempo = 16
         num_tempos = 49
         r = (max_tempo/min_tempo)**(1/(num_tempos-1))
-
         self.tempos = [min_tempo * r**i for i in range(num_tempos)]
         self.np_tempos = np.asarray(self.tempos)
 
-
-        # duration definition
+        # Duration
         np_durations = np.zeros(300)
         i = 0
         note_l = 1/2
-
         for _ in range(int(256/16)):
             while np_durations[i] < note_l:
                 i+=1
                 np_durations[i] = np_durations[i-1] + (note_l/32)
             note_l *= 2
-
         self.np_durations = np_durations[1:129+8]
 
-
-        # position definition
+        # Position
         np_positions = np.zeros(128)
         for i in range(len(np_positions)-1):
             np_positions[i+1] = np_positions[i] + 1/128
-        
         self.np_positions = np_positions
 
-        
-        # time_signature definition
+        # Time signature
         self.numerators = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 132]
         self.denominators = [1, 2, 4, 8, 16, 32, 64, 128]
         self.tot_numerators = len(self.numerators)
         self.tot_denominators = len(self.denominators)
-
+        
+        # Genres
         self.accepted_subgenres = [
             "rock","pop","dance","country","metal",
             "classical","folk","blues","house","indie", 
@@ -76,26 +84,37 @@ class Config:
             "gospel","electronic"
         ]
 
-        self.tf_data_path = os.path.join(self.DATA_PATH, "tf_data")
-        self.lmda_genres_tf_data_path = os.path.join(self.DATA_PATH, "lmda_genres_tf_data")
-
-
-        ### MODEL CONFIGURATIONS
-
-        # DECODER
-        self.SEQ_LEN                        = 1024
+        # MODEL CONFIGURATIONS
+        # Decoder
+        self.SEQ_LEN                        = 6144
         self.TOKEN_DIM                      = 512
-        self.ATTENTION_BLOCKS               = 2
-        self.ATTENTION_HEADS                = 4
+        self.GENRE_DIM                      = 512
+        self.ATTENTION_BLOCKS               = 6
+        self.ATTENTION_HEADS                = 2
         self.DECODER_ACTIVATION_FUNCTION    = "relu"
+
+        # Custom configuration for using GPT2 as a standard transformer decoder
+        self.decoder_config = transformers.GPT2Config(
+            vocab_size=0, 
+            n_positions = self.SEQ_LEN, 
+            n_embd = self.TOKEN_DIM, 
+            n_layer = self.ATTENTION_BLOCKS, 
+            n_head = self.ATTENTION_HEADS, 
+            activation_function='relu'
+        )
 
         # EMBEDDING LAYERS
         self.SINGLE_EMB_SIZE = 64
 
         # DATASET CONFIGURATIONS
-        self.BATCH_SIZE         = 2
+        self.BATCH_SIZE         = 12
+        self.GLOBAL_BATCH_SIZE  = self.BATCH_SIZE * self.num_devices
         self.SHUFFLE_SIZE       = 256
         self.PREFETCH_SIZE      = 32
+
+        # TRAINING SETUP
+        self.REG_LOSS_SCALE  = 0.001
+        self.USE_MASKING     = True
 
         self.INPUT_RANGES = {
             "type": 8,
@@ -126,16 +145,6 @@ class Config:
             tf.keras.layers.Embedding(self.INPUT_RANGES["time_sign"],   self.SINGLE_EMB_SIZE),
             tf.keras.layers.Embedding(self.INPUT_RANGES["tempo"],       self.SINGLE_EMB_SIZE)
         ]
-
-        # Custom configuration for using GPT2 as a standard transformer decoder
-        # self.decoder_config = transformers.GPT2Config(
-        #     vocab_size=0, 
-        #     n_positions = self.SEQ_LEN, 
-        #     n_embd = self.TOKEN_DIM, 
-        #     n_layer = self.ATTENTION_BLOCKS, 
-        #     n_head = self.ATTENTION_HEADS, 
-        #     activation_function='relu'
-        # )
         
         self.output_dense_layers = [
             tf.keras.layers.Dense(self.INPUT_RANGES["type"]),
@@ -179,18 +188,20 @@ class Config:
             np.asarray([True] + [False]*(self.INPUT_RANGES["tempo"]-1), dtype=np.bool8)
         ]
 
-    # def get_decoder(self):
-    #     '''
-    #     Instantiate decoder from its corresponding configuration
-    #     '''
-    #     return transformers.TFGPT2Model(self.decoder_config)
+    def get_decoder(self):
+        '''
+        Instantiate decoder from its corresponding configuration
+        '''
+        return transformers.TFGPT2Model(self.decoder_config)
 
     def get_positional_embedding_matrix(self):
-        # From "Attention is all you need", https://arxiv.org/pdf/1706.03762.pdf
+        '''
+        Obtain the positional encoding matrix for the decoder model.
+        From "Attention is all you need", https://arxiv.org/pdf/1706.03762.pdf
+        '''
         PE = np.zeros((self.SEQ_LEN, self.TOKEN_DIM))
         for pos in range(self.SEQ_LEN):
             for i in range(int(self.TOKEN_DIM/2)):
                 PE[pos,2*i]   = math.sin(pos/(10000**(2*i/self.TOKEN_DIM)))
                 PE[pos,2*i+1] = math.cos(pos/(10000**(2*i/self.TOKEN_DIM)))
         return PE
-
